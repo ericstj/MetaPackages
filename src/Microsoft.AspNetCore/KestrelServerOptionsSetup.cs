2efab50d1fbf6929c658b5add9ebf766357fe09c
John Luo <jt.luo@mail.utoronto.ca>
2017-05-17 14:48:45 -0700

    test, do not merge

--- src/Microsoft.AspNetCore/KestrelServerOptionsSetup.cs
+++ src/Microsoft.AspNetCore/KestrelServerOptionsSetup.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
